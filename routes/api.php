2d64e56443fbaf91ba869c4928badc8e513bdaf0
Xamani <Xamanihamed@gmail.com>
2025-12-30 08:36:59 -0500

    Adds transactions endpoint

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\FinancialReportController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\TransactionController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/orders', [OrderController::class, 'store'])->name('orders.store');
@@ -9,3 +10,5 @@ Route::post('/orders/{order}/pay', [OrderController::class, 'pay'])->name('order
 Route::post('/orders/callback', [OrderController::class, 'callback'])->name('orders.callback');
 
 Route::get('/financial-reports', [FinancialReportController::class, 'index'])->name('financial-reports.index');
+
+Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
