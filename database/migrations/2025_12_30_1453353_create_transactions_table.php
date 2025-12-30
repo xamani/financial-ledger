d0aaa6f84ab8a91ae915f832ba65efef74ef6c00
Xamani <Xamanihamed@gmail.com>
2025-12-30 07:46:10 -0500

    Creates database schema for core entities

---
+++ database/migrations/2025_12_30_1453353_create_transactions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('wallet_id')->constrained()->restrictOnDelete();
+            $table->foreignId('order_id')->nullable()->constrained()->nullOnDelete();
+            $table->string('type')->index();
+            $table->decimal('amount', 18, 0);
+            $table->enum('flow', ['in', 'out']);
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
+
